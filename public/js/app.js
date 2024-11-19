f74d0c703439114abc4c5d6d050e9a164a9b58b3
Abdulvahobjon <abdulvahob0714@gmail.com>
2024-11-19 21:05:35 +0500

    add

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,24 @@ var swiper = new Swiper(".mySwiper", {
     }
 });
 
-
+var swiper = new Swiper(".mySwiperBrend", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".populiar-swiper-button-next",
+        prevEl: ".populiar-swiper-button-prev",
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 6,
+            spaceBetween: 20,
+        },
+    }
+});
 
 let faqBtn  = document.querySelectorAll("#faqBtn")
 let faqInfo  = document.querySelectorAll("#faqInfo")
